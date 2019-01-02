370e772be2a44841fc78b42557ab6b5c6bd00c4d
Forrest Li <forrest.li@kitware.com>
2019-01-02 11:39:39 -0500

    Connect and upload added data

--- src/stores/remote.js
+++ src/stores/remote.js
@@ -10,12 +10,43 @@ function connect(endpoint) {
       // eslint-disable-next-line
       ws = WebsocketConnection.newInstance({ urls: endpoint });
       ws.onConnectionReady(() => resolve(ws.getSession()));
-      ws.onConnectionError(reject);
+      ws.onConnectionError(() => {
+        ws = null;
+        reject();
+      });
       ws.connect();
     } else {
       reject(new Error('empty endpoint'));
     }
   });
+}
+
+/**
+ * Serializes a vtk object and converts all vtkDataArrays into
+ * a wslink binary attachment.
+ */
+function extractAttachments(vtkObj, session) {
+  const state = vtkObj.getState();
+
+  function extract(o) {
+    if (o === null || o === undefined) {
+      return;
+    }
+
+    if (o.vtkClass && o.vtkClass === 'vtkDataArray') {
+      const attachment = new window[o.dataType](o.values).buffer;
+      /* eslint-disable-next-line no-param-reassign */
+      o.values = session.addAttachment(attachment);
+    } else if (typeof o === 'object') {
+      const keys = Object.keys(o);
+      for (let i = 0; i < keys.length; i++) {
+        extract(o[keys[i]]);
+      }
+    }
+  }
+
+  extract(state);
+  return state;
 }
 
 export default {
@@ -24,42 +55,28 @@ export default {
   },
   mutations: {
     SET_REMOTE_ENDPOINT(state, endpoint) {
-      state.endpoint = endpoint || '';
+      state.endpoint = endpoint;
     },
   },
   actions: {
-    UPLOAD_DATA({ rootState, state }) {
-      // connect, then upload current image
-      return connect(state.endpoint)
-        .then((session) => {
-          const source = rootState.proxyManager.getActiveSource();
-          if (source && source.getDataset()) {
-            const serialized = source.getDataset().getState();
-            const findBinary = (o) => {
-              if (o) {
-                if (Array.isArray(o)) {
-                  o.forEach((v) => findBinary(v));
-                } else if (o.constructor === Object) {
-                  if (o.vtkClass && o.vtkClass === 'vtkDataArray') {
-                    // eslint-disable-next-line
-                    o.values = session.addAttachment(
-                      new window[o.dataType](o.values).buffer
-                    );
-                  } else {
-                    Object.keys(o).forEach((k) => findBinary(o[k]));
-                  }
-                }
-              }
-            };
-            findBinary(serialized);
-            console.log(JSON.stringify(serialized).length);
-
-            session.call('upload', [serialized]);
-          }
+    CONNECT({ commit }, endpoint) {
+      return connect(endpoint)
+        .then(() => {
+          commit('SET_REMOTE_ENDPOINT', endpoint || '');
+          // TODO send HELLO
         })
         .catch((error) => {
-          console.error('failed to connect', error);
+          commit('SET_REMOTE_ENDPOINT', '');
+          console.error('Failed to connect:', error);
         });
+    },
+    UPLOAD_DATA({ state }, vtkObj) {
+      return connect(state.endpoint).then((session) => {
+        const obj = extractAttachments(vtkObj, session);
+        session.call('upload', [obj]).then((r) => {
+          console.log('data ID:', r);
+        });
+      });
     },
   },
 };
