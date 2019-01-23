dd36a7d7281a7791de7a00b17a4a441d64382296
Forrest Li <forrest.li@kitware.com>
2019-01-23 13:41:02 -0500

    Add python server

---
+++ server/server.py
@@ -0,0 +1,21 @@
+import sys
+
+from wslink.websocket import ServerProtocol
+from wslink import server
+
+from protocol import Protocol
+
+class AlgorithmServer(ServerProtocol):
+    # TODO change this default secret
+    authKey = 'wslink-secret'
+
+    @staticmethod
+    def configure(options):
+        AlgorithmServer.authKey = options.authKey
+
+    def initialize(self):
+        self.registerLinkProtocol(Protocol())
+        self.updateSecret(AlgorithmServer.authKey)
+
+if __name__ == '__main__':
+    server.start(None, AlgorithmServer)
