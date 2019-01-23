11ba4bd9b5aabce7805955a4c0b332243c80ea80
Forrest Li <forrest.li@kitware.com>
2019-01-23 13:41:02 -0500

    Add python server

---
+++ server/protocol.py
@@ -0,0 +1,20 @@
+import itk
+from wslink import register as rpc
+
+import helper
+
+class Protocol(helper.ObjectProtocol):
+    @rpc('median_filter')
+    @helper.objdir_wrap
+    def median_filter(self, image, radius):
+        itk_image = helper.vtkjs_to_itk_image(image)
+        
+        median_filter = itk.MedianImageFilter[type(itk_image), type(itk_image)].New()
+        median_filter.SetInput(itk_image)
+        median_filter.SetRadius(radius)
+        median_filter.Update()
+
+        result = median_filter.GetOutput()
+
+        # TODO auto-serialize in objdir_wrap?
+        return helper.itk_to_vtkjs_image(result)
